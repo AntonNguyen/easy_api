5de10db7fdefb55063d46181367f6347a92d101b
Anton Nguyen <afnguyen85@gmail.com>
2011-05-15 17:22:05 -0400

    Removed useless files

--- easy_api/easy_api/tests/functional/test_api_user.py
+++
@@ -1,7 +0,0 @@
-from easy_api.tests import *
-
-class TestApiUserController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='api_user', action='index'))
-        # Test response...
--- easy_api/easy_api/tests/functional/test_history.py
+++
@@ -1,7 +0,0 @@
-from easy_api.tests import *
-
-class TestHistoryController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='history', action='index'))
-        # Test response...
